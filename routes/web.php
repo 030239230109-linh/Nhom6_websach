f23bb377ac866c5eb32de71ccc50e858bc16d570
Thuylinh117 <030239230111@st.buh.edu.vn>
2026-03-18 20:58:50 +0700

    add feature top budget movies

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 
 Route::get('/nguyenhuynhthuylinh', function () {
     return "Nguyễn Huỳnh Thùy Linh";
 });
-=======
-<<<<<<< HEAD
 
 Route::get('/truonghomailinh', function () {
     return "Trương Hồ Mai Linh";
@@ -24,13 +23,18 @@ Route::get("/qlsach/theloai","App\Http\Controllers\BookController@laythongtinthe
 Route::get("/qlsach/thongtinsach","App\Http\Controllers\BookController@laythongtinsach");
 
 Route::get("/qlsach/themtheloai","App\Http\Controllers\BookController1@themtheloai");
-<<<<<<< HEAD
 
 Route::get("/lehoangan", function() {return 'Lê Hoàng An';});
-=======
+
 Route::get('/tenban', function () {
-    return 'Le Nguyen Nhu Quynh - Nhóm 6';
-});
->>>>>>> cfce62b717191661e1f0606b085d69df2fa14a6a
->>>>>>> 9c0a3fa1167db62900147759931d826ed9151df4
->>>>>>> 6fafaec210299e4ee7f2ce346335a6e943c33641
+    return 'Le Nguyen Nhu Quynh - Nhóm 6';});
+
+// Đường dẫn hiển thị 10 phim doanh thu cao nhất
+Route::get('/73_top_budget', function () {
+    $movies = DB::table('movie')
+        ->orderBy('budget', 'desc')
+        ->limit(10)
+        ->get();
+
+    return view('73_top_budget', compact('movies'));
+}); 
