9eac9c6a6309b531ebe8508ff9ab9466253d1d09
lllinh <030239230109@st.buh.edu.vn>
2026-03-18 15:55:14 +0700

    Nguyen huynh thuy linh

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\bookController;
 
-Route::get('/kiemtra1','App\Http\Controllers\kiemtra1Controller@kiemtra1'); 
-Route::post('/tinhtuoi','App\Http\Controllers\kiemtra1Controller@tinhtuoi');
-
-Route::get("/qlsach/theloai","App\Http\Controllers\BookController@laythongtintheloai");
-Route::get("/qlsach/thongtinsach","App\Http\Controllers\BookController@laythongtinsach");
-
-Route::get("/qlsach/themtheloai","App\Http\Controllers\BookController1@themtheloai");
+Route::get('/nguyenhuynhthuylinh', function () {
+    return "Nguyễn Huỳnh Thùy Linh";
+});
