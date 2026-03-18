78caf1ca47f9fa67c51f100bb0204926421bd26a
lllinh <030239230109@st.buh.edu.vn>
2026-03-18 21:12:42 +0700

    sua

--- routes/web.php
+++ routes/web.php
@@ -9,24 +9,12 @@ Route::get('/nguyenhuynhthuylinh', function () {
     return "Nguyễn Huỳnh Thùy Linh";
 });
 
-
 Route::get('/truonghomailinh', function () {
     return "Trương Hồ Mai Linh";
 });
 Route::get('/nguyenthithuylinh', function () {
     dd("Nguyen Thi Thuy Linh");
 });
-
-<<<<<<< HEAD
-=======
-
-Route::get('/kiemtra1','App\Http\Controllers\kiemtra1Controller@kiemtra1'); 
-Route::post('/tinhtuoi','App\Http\Controllers\kiemtra1Controller@tinhtuoi');
-
-Route::get("/qlsach/heloai","App\Http\Controllers\BookController@laythongtintheloai");
-Route::get("/qlsach/thongtinsach","App\Http\Controllers\BookController@laythongtinsach");
-
-Route::get("/qlsach/themtheloai","App\Http\Controllers\BookController1@themtheloai");
 
 Route::get("/lehoangan", function() {return 'Lê Hoàng An';});
 
@@ -36,9 +24,10 @@ Route::get('/tenban', function () {
 
 Route::get('/lethihonglinh', function () {
     dd("Le Thi Hong Linh");
+});
 
 Route::get('/nguyenhuynhsaly', function () {
     return "Nguyễn Huỳnh Sa Ly";
 
 });
->>>>>>> b039f7fe3ddc8968e9791c213b8d31817f483ae9
+
