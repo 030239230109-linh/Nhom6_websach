8f64e96178ea5114878bb9a85b2f474653d26a96
meillinhst-cpu <meillinh.st@gmail.com>
2026-03-18 15:53:01 +0700

    truong ho mai linh

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\bookController;
 
-Route::get('/kiemtra1','App\Http\Controllers\kiemtra1Controller@kiemtra1'); 
-Route::post('/tinhtuoi','App\Http\Controllers\kiemtra1Controller@tinhtuoi');
 
-Route::get("/qlsach/theloai","App\Http\Controllers\BookController@laythongtintheloai");
-Route::get("/qlsach/thongtinsach","App\Http\Controllers\BookController@laythongtinsach");
-
-Route::get("/qlsach/themtheloai","App\Http\Controllers\BookController1@themtheloai");
+Route::get('/truonghomailinh', function () {
+    return "Trương Hồ Mai Linh";
+});
