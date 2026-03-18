697cedf6e946609f5b0593c38288a5f8f00adfd8
Thuylinh117 <030239230111@st.buh.edu.vn>
2026-03-18 15:44:25 +0700

    add route nguyenthithuylinh

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\bookController;
 
+Route::get('/nguyenthithuylinh', function () {
+    dd("Nguyen Thi Thuy Linh");
+});
+
 Route::get('/kiemtra1','App\Http\Controllers\kiemtra1Controller@kiemtra1'); 
 Route::post('/tinhtuoi','App\Http\Controllers\kiemtra1Controller@tinhtuoi');
 
