e4f36b28d64f3bf25733a8692b7f9c10fc657103
nq-hub <030239230202@st.buh.edu.vn>
2026-03-18 17:04:30 +0700

    them routes

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
-
-
-Route::get('/nguyenhuynhthuylinh', function () {
-    return "Nguyễn Huỳnh Thùy Linh";
-});
-=======
-<<<<<<< HEAD
-
-Route::get('/truonghomailinh', function () {
-    return "Trương Hồ Mai Linh";
-});
-Route::get('/nguyenthithuylinh', function () {
-    dd("Nguyen Thi Thuy Linh");
+// 1. Route mặc định của Laravel
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/kiemtra1','App\Http\Controllers\kiemtra1Controller@kiemtra1'); 
-Route::post('/tinhtuoi','App\Http\Controllers\kiemtra1Controller@tinhtuoi');
-
-Route::get("/qlsach/theloai","App\Http\Controllers\BookController@laythongtintheloai");
-Route::get("/qlsach/thongtinsach","App\Http\Controllers\BookController@laythongtinsach");
-
-Route::get("/qlsach/themtheloai","App\Http\Controllers\BookController1@themtheloai");
-<<<<<<< HEAD
-
-Route::get("/lehoangan", function() {return 'Lê Hoàng An';});
-=======
-Route::get('/tenban', function () {
-    return 'Le Nguyen Nhu Quynh - Nhóm 6';
+// 2. Route cá nhân (Bước 3 - Nhánh main)
+Route::get('/lnnq', function () {
+    return 'lenguyennhuquynh - Nhóm 6';
 });
->>>>>>> cfce62b717191661e1f0606b085d69df2fa14a6a
->>>>>>> 9c0a3fa1167db62900147759931d826ed9151df4
->>>>>>> 6fafaec210299e4ee7f2ce346335a6e943c33641
+
+// 3. Route xử lý Database (Bước 7 - Nhánh riêng)
+// Đảm bảo bạn đã chạy lệnh: php artisan make:controller MovieController
+Route::get('/the-loai', [MovieController::class, 'getGenres']);
