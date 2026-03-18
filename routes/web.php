1d139df2c894ff29f87d2891d79dadc4783cb002
Thuylinh117 <030239230111@st.buh.edu.vn>
2026-03-18 16:41:19 +0700

    add route nguyenthithuylinh

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,3 @@ Route::get('/nguyenthithuylinh', function () {
     dd("Nguyen Thi Thuy Linh");
 });
 
-Route::get('/kiemtra1','App\Http\Controllers\kiemtra1Controller@kiemtra1'); 
-Route::post('/tinhtuoi','App\Http\Controllers\kiemtra1Controller@tinhtuoi');
-
-Route::get("/qlsach/theloai","App\Http\Controllers\BookController@laythongtintheloai");
-Route::get("/qlsach/thongtinsach","App\Http\Controllers\BookController@laythongtinsach");
-
-Route::get("/qlsach/themtheloai","App\Http\Controllers\BookController1@themtheloai");
-Route::get('/tenban', function () {
-    return 'Le Nguyen Nhu Quynh - Nhóm 6';
-});
