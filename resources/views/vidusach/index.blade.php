eb5ba9bfee5e1b85e0ff1de35ef6eb7b93955a28
lllinh <030239230109@st.buh.edu.vn>
2026-03-25 16:25:17 +0700

    moi

---
+++ resources/views/vidusach/index.blade.php
@@ -0,0 +1,24 @@
+<x-book-layout>
+    <x-slot name = 'title'>
+        Sách
+</x-slot>
+
+@section("title","Sách")
+
+@section("content")
+
+<div class="list-book">
+    @foreach($data as $row)
+        <div class="book">
+                    <a href="{{url('sach/chitiet/'.$row->id)}}">
+
+            <img src="{{ asset('images/'.$row->file_anh_bia) }}" 
+                 width="200px" height="200px"><br>
+
+            <b>{{ $row->tieu_de }}</b><br/>
+
+            <i>{{ number_format($row->gia_ban, 0, ",", ".") }}đ</i>
+        </div>
+    @endforeach
+</div>
+</x-book-layuot>
