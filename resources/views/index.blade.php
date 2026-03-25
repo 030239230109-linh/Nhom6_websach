2e42f4af8f7328f6528eeb92a6b7a489ca9aa356
LeHoangAn-MIS <030239230002@st.buh.edu.vn>
2026-03-25 20:31:10 +0700

    merge nq

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,15 @@
 <x-book-layout>
+    <x-slot name='title'>
+        Sách
+    </x-slot>
 
-<x-slot name='title'>
-    Sách
-</x-slot>
-
-
-
-<h4>Danh sách sách</h4>
-
-<div class="list-book">
+<div class='list-book'>
     @foreach($data as $row)
-        <div class="book">
-            <a href="{{ url('sach/chitiet/'.$row->id) }}">
-                <img src="{{ asset('book_image/'.$row->file_anh_bia) }}" width="200"><br>
-                <b>{{$row->tieu_de}}</b><br>
-                <i>{{number_format($row->gia_ban,0,",",".")}} đ</i>
+        <div class='book'>
+            <a href="{{url('sach/chitiet/'.$row->id)}}">
+                <img src="{{asset('hinh/image/'.$row->file_anh_bia)}}" width='200px' height='200px'><br>
+                <b>{{$row->tieu_de}}</b><br/>
+                <i>{{number_format($row->gia_ban,0,",",".")}}đ</i>
             </a>
         </div>
     @endforeach
