09d2613ec1b475322ba49b54601b66144e4c5831
nq-hub <030239230202@st.buh.edu.vn>
2026-03-30 09:01:49 +0700

    pull thuc hanh 2

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
-use App\Http\Controllers\BookController;
 
-// Trang mặc định
-Route::get('/sach', [BookController::class, 'sach']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Lọc theo thể loại
-Route::get('/sach/theloai/{id}', [BookController::class, 'theloai']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Chi tiết sách
-Route::get('sach/chitiet/{id}','App\Http\Controllers\BookController@chitiet');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
