dd8412247cbc34d15db477a381edb23a9df1e8ed
lllinh <030239230109@st.buh.edu.vn>
2026-03-31 20:57:30 +0700

    kk

--- app/Http/Controllers/ViduLayoutController.php
+++ app/Http/Controllers/ViduLayoutController.php
@@ -3,24 +3,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 class ViduLayoutController extends Controller
 {
     function sach()
     {
-    $data = DB::select("select * from sach order by gia_ban asc limit 0,8");
-    return view("vidusach.index", compact("data"));
+        $data = DB::select("select * from sach order by gia_ban asc limit 0,8");
+        return view("vidusach.index", compact("data"));
     }
-function theloai($id)
-{
-$data = DB::select("select * from sach where the_loai = ?",[$id]);
-return view("vidusach.index", compact("data"));
-}
-function chitiet($id)
-{
-    $data = DB::select("select * from sach where id = ?", [$id]);
 
-    return view("vidusach.chitiet", [
-        "book" => $data[0] // lấy phần tử đầu
-    ]);
-}
+    function theloai($id)
+    {
+        $data = DB::select("select * from sach where the_loai = ?", [$id]);
+        return view("vidusach.index", compact("data"));
+    }
+
+    public function chitiet($id)
+    {
+        $book = DB::select("select * from sach where id = ?", [$id]);
+
+        return view("vidusach.chitiet", [
+            "book" => $book[0] // lấy phần tử đầu
+        ]);
+    }
 }
